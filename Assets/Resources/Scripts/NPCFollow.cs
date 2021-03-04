eb5d9d9da14813091b2e2cbc21b8c37e355a926f
Keja0809 <kerstinlimberger@gmx.at>
2021-03-04 16:44:28 +0100

    Scene 2 Update
    First Dialog n Scene 2
    Script to NPC Follow Up - without animation until now
    only on Character Chris

---
+++ Assets/Resources/Scripts/NPCFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFollow : MonoBehaviour
+{
+
+    public GameObject ThePlayer;
+    public float TargetDistance;
+    public float AllowedDistance = 5;
+    public GameObject TheNPC;
+    public float FollowSpeed;
+    public RaycastHit Shot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(ThePlayer.transform);
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out Shot))
+        {
+            TargetDistance = Shot.distance;
+            if(TargetDistance >= AllowedDistance)
+            {
+                FollowSpeed = 0.1f;
+                TheNPC.GetComponent<Animation>().Play("Basic_Walk_01_Root");
+                transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, FollowSpeed);
+            }
+            else
+            {
+                FollowSpeed = 0;
+                TheNPC.GetComponent<Animation>().Play("HumanoidIdle");
+            }
+        }
+        
+    }
+}
